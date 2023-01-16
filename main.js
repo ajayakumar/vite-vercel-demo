2a5f5359a31e67929632be5e3fe2d2ea2c8678b5
Ajay <ajayakumar.s@gmail.com>
2023-01-17 00:45:54 +0530

    form and style sheets implemented

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js'
-
+/*
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -15,9 +15,8 @@ document.querySelector('#app').innerHTML = `
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite logo to learn more now
+      Click on the Vite logo to learn more
     </p>
   </div>
 `
-
-setupCounter(document.querySelector('#counter'))
+*/
